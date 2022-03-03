a75e6a14c4ede15c1206d44431efc07decfb91a3
quaxsze <plum.stanislas@gmail.com>
2022-03-03 18:15:48 +0100

    add template

--- udata_transport/views.py
+++ udata_transport/views.py
@@ -7,6 +7,12 @@ from udata_front.frontend import template_hook
 blueprint = Blueprint('transport', __name__, template_folder='templates')
 
 
-@template_hook()
+def has_transport_url(ctx):
+    dataset = ctx['dataset']
+    return dataset and dataset.extras.get('transport_url', '')
+
+
+@template_hook('dataset.display.transport-banner', when=has_transport_url)
 def dataset_recommendations(ctx):
-    return theme.render()
+    transport_url = ctx['dataset'].extras.get('transport_url', '')
+    return theme.render('dataset-transport.html', transport_url=transport_url)
