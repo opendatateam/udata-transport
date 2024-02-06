497f4f542524ab4e7ba7153f5ca8c600e342b93b
maudetes <maudet.estelle@gmail.com>
2024-02-06 10:27:30 +0100

    Clear datasets only after successful request
    It prevents removing existing extras on failure

--- udata_transport/tasks.py
+++ udata_transport/tasks.py
@@ -14,13 +14,15 @@ def process_dataset(dataset):
     target_dataset.save()
 
 
-@job("map-transport-datasets")
-def map_transport_datasets(self):
+def clear_datasets():
     datasets = Dataset.objects(**{'extras__transport:url__exists': True}).no_cache().timeout(False)
     for dataset in datasets:
         dataset.extras.pop('transport:url', None)
         dataset.save()
 
+
+@job("map-transport-datasets")
+def map_transport_datasets(self):
     source = current_app.config.get('TRANSPORT_DATASETS_URL', None)
     if not source:
         error('TRANSPORT_DATASETS_URL variable must be set.')
@@ -31,6 +33,7 @@ def map_transport_datasets(self):
         error('Remote platform unreachable.')
         return
     results_list = response.json()
+    clear_datasets()
     for dataset in results_list:
         process_dataset(dataset)
     success(f'Done. {len(results_list)} datasets mapped to transport')
